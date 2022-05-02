ecd2c4884699560905f6c4be57063aa691195500
ZhengfeizZZ <349565531@gapps.yrdsb.ca>
2022-05-02 13:52:59 -0400

    New changes: .isKeyDown()

--- MyWorld.java
+++ MyWorld.java
@@ -1,14 +1,9 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class MyWorld here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
  */
 public class MyWorld extends World
 {
-
     /**
      * Constructor for objects of class MyWorld.
      * 
@@ -16,17 +11,9 @@ public class MyWorld extends World
     public MyWorld()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(600, 400, 1); 
-
-        Pig p = new Pig("Piggy");
+        super(600, 400, 1);
+        
+        Pig p = new Pig();
         addObject(p, 300, 200);
-        prepare();
-    }
-    /**
-     * Prepare the world for the start of the program.
-     * That is: create the initial objects and add them to the world.
-     */
-    private void prepare()
-    {
     }
 }
